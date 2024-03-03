ff3960ed283f0ccced641b9784ae716d83888eac
Duygu <duyguasenakaya@gmail.com>
2024-03-03 20:52:04 +0300

    init project

---
+++ src/main/java/com/example/MovieFriend/MovieFriendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MovieFriend/MovieFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
